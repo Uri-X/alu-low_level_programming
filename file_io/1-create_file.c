1afd22f9c65e36602090f9e749689ffb407112f2
Philbert Kuria <philbertkuria@gmail.com>
2026-01-11 15:28:41 +0300

    tread carefully

---
+++ file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: pointer to filename
+ * @text_content: string to write to file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file, i;
+	ssize_t wr;
+
+	if (filename == NULL)
+		return (-1);
+
+	file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (file == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		i = 0;
+		while (text_content[i])
+			i++;
+
+		wr = write(file, text_content, i);
+		if (wr == -1 || wr != i)
+		{
+			close(file);
+			return (-1);
+		}
+	}
+
+	close(file);
+	return (1);
+}
+
